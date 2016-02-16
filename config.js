557ddd8dd848d161123b7bee6ba9ec3ea0032988
tranle <lettm@soft-flight.com>
2016-02-16 10:12:47 +0700

    change again

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AIzaSyAkUqaZokVNU_NfhjEUBY1iNQBKZ1NWKMg'
+  gcmAPIKey: 'AIzaSyCHatkPeXSRebCnVVi1e0mtkYQ9IL0vB3g'
 };
